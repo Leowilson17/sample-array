2f5a7d5622de71d1d63d0ffe10b3feeae79f409a
leowilson <leobca2018@gmail.com>
2022-07-12 11:09:10 +0530

    add-script-file.

--- find-greater-num/script.js
+++ find-greater-num/script.js
@@ -6,12 +6,16 @@ for (var k = 0; k < howManyNumbers; k++) {
     a.push(inputNUmbers);
 }
 console.log(a);
-for (var i = 0; i < a.length; i++) {
-    //console.log(a[i]);
-    for (var j = 0; j < a.length; j++) {
-        if (a[i] > a[j]) {
-            var store = a[i];
-        }
-    }
+var show = 0;
+var pass = 0;
+for ( i = 0; i < a.length;i++){
+for ( j = 0; j < a.length;j++){
+if(a[i] > a[j]){  
+   show = a[i];
+   if(show > pass){
+   pass = show;
+   }
 }
-console.log(store);
+}
+} 
+console.log(pass);
